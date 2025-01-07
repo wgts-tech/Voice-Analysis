352fda82b524d80667b7cfbed631d93377609a52
wgts-tech <mm_amini@wgts.tech>
2025-01-07 09:21:15 +0330

    Update and rename Whisper_Words_Time.py to Whisper_Words_Time_Calc.py

--- voice_record_and_analyze_gui/Whisper_Words_Time_Calc.py
+++ voice_record_and_analyze_gui/Whisper_Words_Time_Calc.py
@@ -1,20 +1,17 @@
 import torch
 import whisper
 
-# بررسی اینکه GPU در دسترس است
 if torch.cuda.is_available():
     print("CUDA is available. Using GPU.")
 else:
     print("CUDA is not available. Using CPU.")
 
-# بارگذاری مدل Whisper
-#model = whisper.load_model("medium").to("cuda") # مدل‌های دیگر: "tiny", "small", "medium", "large"
+
+#model = whisper.load_model("medium").to("cuda")
 model = whisper.load_model("medium", device="cuda" if torch.cuda.is_available() else "cpu")
 
-# پردازش فایل صوتی
 result = model.transcribe("temp_audio.wav", word_timestamps=True)
 
-# نمایش کلمات همراه با بازه‌های زمانی
 for segment in result["segments"]:
     print(f"Segment: {segment['text']}")
     for word in segment["words"]:
